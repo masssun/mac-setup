7a0aaab3d033afd3f0a309d9cd3bc437cc65a418
hofzzy <hofzzy@gmail.com>
2021-04-05 13:13:41 +0900

    For iMac

--- setup.sh
+++ setup.sh
@@ -1,12 +1,31 @@
 ########################################    app list    ########################################
 apps=(
   alfred
-  atom
-  firefox
+  android-studio
+  deepl
   google-chrome
   google-japanese-ime
   iterm2
+  slack
   visual-studio-code
+)
+
+brews=(
+  dart
+  ffmpeg
+  gh
+  ghq
+  go
+  nodenv
+  rbenv
+  ruby-build
+  peco
+  pyenv
+  zsh
+)
+
+pubs=(
+  fvm
 )
 
 gems=(
@@ -16,6 +35,7 @@ gems=(
 git_configs=(
   "user.name hofzzy"
   "user.email hofzzy@gmail.com"
+  "ghq.root ~/.ghq"
 )
 
 ######################################## End of app list ########################################
@@ -47,7 +67,7 @@ if test ! $(which brew); then
   }
   prompt "Install Xcode" install_xcode
   install_homebrew () {
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   }
   prompt "Install Homebrew" install_homebrew
 else
@@ -57,20 +77,24 @@ else
   prompt "Update Homebrew" update_homebrew
 fi
 
-install_brew_cask () {
-  brew install caskroom/cask/brew-cask
-}
-prompt "Install Homebrew-Cask" install_brew_cask
-
 install_apps () {
-  brew cask install --appdir="/Applications" ${apps[@]}
+  brew install --cask --appdir="/Applications" ${apps[@]}
 }
 prompt "Install apps with Cask" install_apps
 
-install_zsh () {
-  brew install zsh
+install_brews () {
+  brew tap dart-lang/dart
+  brew install ${brews[@]}
 }
-prompt "Install Zsh" install_zsh
+prompt "Install Homebrew packages" install_brews
+
+install_dart_packages () {
+  pub global activate ${pubs[@]}
+}
+prompt "Install Dart packages" install_dart_packages
+
+# Change the login shell to zsh
+# chsh -s /bin/zsh
 
 install_oh_my_zsh () {
   curl -L http://install.ohmyz.sh | sh
@@ -81,7 +105,7 @@ create_ssh_key () {
   dir=$HOME/.ssh; [ ! -e $dir ] && mkdir -p $dir
   ssh-keygen -t rsa
   eval `ssh-agent`
-  ssh-add $HOME/.ssh/id_rsa
+  ssh-add -K
 }
 configure_git () {
   create_ssh_key
@@ -90,6 +114,7 @@ configure_git () {
     git config --global ${config}
   done
   git remote set-url origin git@github.com:
+  echo "Host github.com\nHostName github.com\nUser git\nIdentityFile ~/.ssh/id_rsa" >> ~/.ssh/config
 }
 prompt "Coufigure git defaults" configure_git
 
